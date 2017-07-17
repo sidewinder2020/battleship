bcc161e833ca4feebf713d9dfd41d02544522b22
sidewinder2020 <sdwinder1@gmail.com>
2017-07-17 14:39:23 -0600

    Add 1 method to game class, and Create message module

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,28 @@ require 'pry'
 require './lib/computer'
 require './lib/player'
 
+include messages.rb
+
 class Game
 attr_accessor :computer, :player
 
   def initialize
     @computer = Computer.new
     @player = Player.new
+  end
+
+  def start_game
+    opening_message
+    player_choice = gets.chomp
+    if player_choice == "p"
+      #next method
+    elsif player_choice == "i"
+      instructions
+    elsif player_choice == "q"
+      exit
+    else
+      "Wrong sucka! Try again!"
+    end
   end
 
   def shoot(coord)
---
+++ lib/messages.rb
@@ -0,0 +1,11 @@
+module Messages
+
+  def opening_message
+    puts "Welcome to Battleship!/n Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  end
+
+  def instructions
+    puts 
+  end
+
+end
