a0f5115272dd38f7067e86476118e7bff9556797
sidewinder2020 <sdwinder1@gmail.com>
2017-07-18 16:24:41 -0600

    Create ship validation methods within game class, entered more messages into the message module

--- lib/game.rb
+++ lib/game.rb
@@ -26,6 +26,50 @@ attr_accessor :computer, :player
     end
   end
 
+  def comp_places_ships
+  # computer places ships, validates both
+  # notifies player that computer ships have been placed, prompts player for coords for 2-space ship
+  computer.second_ship
+  comp_places_ships_msg
+  end
+
+  def player_places_ships(coord_1, coord_2)
+  # coords for 2-space ship are entered and validated
+  # prompts player for coords for 3-space ship
+  validate_coord_1(coord_1)
+  validate_coord_2(coord_1, coord_2)
+  player_prompted_for_3rd_ship_msg
+  end
+
+  def player_places_second_ship
+  # coords for 3-space ship are entered and validated
+  validate_ship_2_coord_1(coord_1)
+  validate_ship_2_coord_2(coord_1, coord_2)
+  validate_ship_2_coord_3(coord_1, coord_2, coord_3)
+  game_begins_msg
+  end
+
+  # game begins
+
+  def player_fire_sequence
+  # display map, from player's point of view, and prompt player for position on which to fire
+  request_fire_coordinates_from_player_msg
+  end
+
+  # indicate hit or miss - show updated map [hit becomes 'H', miss becomes 'M']
+
+  # prompt player to hit enter
+
+  # computer fires @ random
+
+  # display player's map with H or M, where the computer fired
+
+  # (need a passive method to check if all of the squares are holding a value of false - if so, then game over) - this method will be used IMMEDIATELY AFTER the player fires, and again after the computer fires.
+
+  # (need a passive method to iterate through an array of the ship's coords and check if any are true) - if there are any true values, do nothing, if there aren't, put 2 - space ship is sunk
+
+  # (do the previous method)
+
   def shoot(coord)
     if coord == false
       puts "miss!"
--- lib/messages.rb
+++ lib/messages.rb
@@ -5,7 +5,24 @@ module Messages
   end
 
   def instructions
-    puts 
+    puts ""
+  end
+
+  def comp_places_ships_msg
+    puts "Computer ships have been placed./n Please place your first 2-space ship following the pattern: A1 B1 (or your first and second coordinates with a space separating them)."
+  end
+
+  def player_prompted_for_3rd_ship_msg
+    "Ok, you've placed your first ship, now it's time to place your second.  This one will take up 3 spaces, and requires 3 coordinates.  The coordinates must be:/n
+    -next to eachother/n -in a horizontal OR vertical line/n -entered in the order in which they appear on the board/n ex: A1 B1 C1/n  You CANNOT overlap your ships."
+  end
+
+  def game_begins_msg
+    "You have placed all your ships, and now.../n the game begins."
+  end
+
+  def request_fire_coordinates_from_player_msg
+    
   end
 
 end
